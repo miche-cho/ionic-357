a23e28e13952534488cfe328b678a3cbc860fbd1
miche-cho <michelle.choi@mail.mcgill.ca>
2020-04-26 00:26:11 -0400

    setup

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,18 +1,29 @@
+// home.module.ts
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+ 
 import { HomePage } from './home.page';
-
-import { HomePageRoutingModule } from './home-routing.module';
-
-
+ 
+import { AgmCoreModule } from '@agm/core';
+ 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage
+      }
+    ]),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAiJRnaj371Ugz4X9L176JNamY8CWfqhVc', 
+      libraries: ['places']
+    })
   ],
   declarations: [HomePage]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,109 @@
-import { Component } from '@angular/core';
-
+// home.page.ts
+import { Component, OnInit, ViewChild, ElementRef, NgZone, AfterViewInit } from '@angular/core';
+import { MapsAPILoader, AgmMap } from '@agm/core';
+ 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage  implements OnInit {
+  title: string = 'AGM project';
+  latitude: number;
+  longitude: number;
+  zoom: number;
+  address: string;
+  private geoCoder;
+  readonly mapRadius: number = 0.3;
+  @ViewChild(AgmMap,{static:true})
+  public agmMap: AgmMap
+  @ViewChild('search',{static:false})
+  public searchElementRef: ElementRef;
+  currentLat: number;
+  currentLng: number;
+ 
+  constructor(
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone
+  ) { }
+ 
+ 
+  ngOnInit() {
+    //load Places Autocomplete
+    this.mapsAPILoader.load().then(() => {
+      this.setCurrentLocation();
+      const nwBounds = new google.maps.LatLng({
+      lat: this.currentLat - this.mapRadius,
+      lng: this.currentLng - this.mapRadius,
+      });
+      const seBounds = new google.maps.LatLng({
+      lat: this.currentLat + this.mapRadius,
+      lng: this.currentLng + this.mapRadius,
+      });
+      
+      this.geoCoder = new google.maps.Geocoder();
+ 
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+        bounds: new google.maps.LatLngBounds(nwBounds, seBounds)
+      });
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          //get the place result
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+ 
+          //verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+ 
+          //set latitude, longitude and zoom
+          this.latitude = place.geometry.location.lat();
+          this.longitude = place.geometry.location.lng();
+          this.zoom = 12;
+        });
+      });
+    });
+  }
+ 
 
-  constructor() {}
-
+  // Get Current Location Coordinates
+  private setCurrentLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.currentLng = position.coords.longitude;
+        this.currentLat = position.coords.latitude;
+        this.zoom = 8;
+        this.getAddress(this.latitude, this.longitude);
+      });
+    }
+  }
+ 
+ 
+  markerDragEnd($event: any) {
+    console.log($event);
+    this.latitude = $event.coords.lat;
+    this.longitude = $event.coords.lng;
+    this.getAddress(this.latitude, this.longitude);
+  }
+ 
+  getAddress(latitude, longitude) {
+    this.geoCoder.geocode({ 'location': { lat: latitude, lng: longitude } }, (results, status) => {
+      console.log(results);
+      console.log(status);
+      if (status === 'OK') {
+        if (results[0]) {
+          this.zoom = 12;
+          this.address = results[0].formatted_address;
+        } else {
+          window.alert('No results found');
+        }
+      } else {
+        window.alert('Geocoder failed due to: ' + status);
+      }
+ 
+    });
+  }
+ 
 }
